7b8c7802d3ce2e81477f9b2659f1c981e286ef5a
ccf23 <ccf23@pitt.edu>
2020-11-08 16:23:32 -0500

    integrating destination into RAT

--- RAT.py
+++ RAT.py
@@ -1,3 +1,49 @@
+from functional_units import *
+
+print("Testing operation of all classes defined in functional_units.py")
+
+# Initialize the processor and all functional units
+instruction_buffer = InstructionBuffer(r"C:\Users\HP\github\ca_semester_project\input.txt")
+#instruction_buffer = InstructionBuffer("input.txt")
+
+for i, instruction in enumerate(instruction_buffer):
+    print("i = {}: value = {}".format(i, instruction))
+    print(instruction.__dict__)
+    print("Instruction rs: {}".format(instruction.rs))
+
+int_adder = IntegerAdder(3, 2, 1)
+
+# Issue instruction to fp_adder functional unit
+
+int_adder.issue({"op":"ADD","vj":10, "vk":5, "qj":None, "qk":None, "dest":"ROB1"})
+
+int_adder.issue({"op":"ADD","vj":3, "vk":6, "qj":None, "qk":None, "dest":"ROB2"})
+
+int_adder.tick()
+
+int_adder.tick()
+
+int_adder.tick()
+
+int_adder.tick()
+
+int_adder.tick()
+
+int_adder.tick()
+
+int_adder.tick()
+
+int_adder.tick()
+
+
+# This will give you a dictionary: for example, {"dest","ROB1":"answer":10.3} would be the output for an operation with
+# destination ROB1 and value 10.3
+result = int_adder.deliver()
+print("First result: {}".format(result))
+print(result["dest"])
+print(result["answer"])
+
+
 # create free pool
 # make 101 trasnsition registers (will probably not need more)
 # transition registers from freepool are differentiated by 'X'
