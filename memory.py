4f7149c0dc389c7b02530ced9cc588a2ba5990b2
Brendan L <bkl12@pitt.edu>
2020-11-08 23:28:42 -0500

    Bug Fixes to memory / LSQ

--- memory.py
+++ memory.py
@@ -1,22 +1,34 @@
 # Memory Element classes
 # ========================
 
-#Memory inits with the spec'd size, and will fit the value range to it, if provided
-#The value 0x0 is reserved as a NULL ref.
-#  If called by str, NONE is always returned.
-#  If called by ld, the value is dumped.
+"""
+    Load Store Queue
 
+    Core Memory Block
+    - Block is parameterizable in the following ways:
+    - size_bytes: Size of the member available in bytes, total bytes avialable to store
+    - word_len=4: Length of the word. Though the actual array doesn't care, the class
+        forces effective addresses to align to a word length
+
+    - Number of elements available is size_bytes / word_len
+    - Class cannot init if size_bytes % word_len != 0
+    - To think in terms of elements, not bytes, set word_len = 1.
+"""
 from functional_units import *
 
+
 class LoadStoreQueue:
-    def __init__(self, mem_size, queue_len, cycles_in_mem, verbose=False, config=None):
+    def __init__(self, mem_size, queue_len, cycles_in_mem, rob, verbose=False, config=None, wl=4):
+        # hardware params
         self.verbose = verbose
         self.num_stats_free = queue_len
         self.cycles_in_mem = cycles_in_mem
-
+        #sub-component params
         self.queue_stations = [] * queue_len
-        self.mem_unit = Memory(mem_size, verbose=verbose, mem_config=config)
-
+        self.result_buffer = []
+        self.mem_unit = Memory(mem_size, word_len=wl, mem_config=config)
+        #component ref params
+        self.reorder_buffer = rob
 
     def issue(self, instr):
         if self.num_stats_free == 0:
@@ -25,12 +37,14 @@ class LoadStoreQueue:
         #
         enqueue = {"op":instr["op"], "qrs":instr["rs"], "qrt":instr["rt"], \
                    "vrs":None, "vrt":None, "imm":instr["imm"], \
-                   "countdown":self.cycles_in_mem+1, "commit":False}
+                   "countdown":self.cycles_in_mem, "commit":False}
 
         if enqueue["op"] == "Ld":
             enqueue["commit"] = True
-        #request value from register for rt
-        #request value from register for rs
+
+        enqueue["vrs"] = self.reorder_buffer.request(enqueue["qrs"])
+        enqueue["vrt"] = self.reorder_buffer.request(enqueue["qrt"])
+
         self.num_stats_free -= 1
         self.queue_stations.append(enqueue)
 
@@ -40,19 +54,21 @@ class LoadStoreQueue:
         if self.num_stats_free == len(self.queue_stations):
             # if nothing is queue'd, nothing to do.
             return
-"""
-        WE SHOULDNT NEED TO CHECK FOR VALUES HERE
-           ROB is read on "issue" of new instr. to queue
-           Arch. Registers are also read on "issue" and no values have to wait.
-           CBD will automatically push values to us (and the rob, but we already have them)
-          We checked all sources in due time, so no need to repeat.
-"""
+
         queue_leader = self.queue_stations[0]
-        if lsq_entry_ready(queue_leader) and self.result_buffer[0] is None:
+        if lsq_entry_ready(queue_leader) and len(self.result_buffer) == 0:
             # register is ready to go to mem. & not waiting to tender a result
-            if(queue_leader["countdown"] -= 1) == 0:
+            """
+            WE SHOULDNT NEED TO CHECK FOR VALUES HERE
+                ROB is read on "issue" of new instr. to queue
+                Arch. Registers are also read on "issue" and no values have to wait.
+                CBD will automatically push values to us (and the rob, but we already have them)
+                We checked all sources in due time, so no need to repeat.
+            """
+            queue_leader["countdown"] -= 1
+            if (queue_leader["countdown"]) == 0:
                 # calc effective addres
-                eff_addr = queue_leader["vrt"] + queue_leader["imm"]
+                eff_addr = queue_leader["vrt"] + int(queue_leader["imm"])
                 # act on memory
                 res = self.mem_unit.access(queue_leader["op"], eff_addr, queue_leader["vrs"])
 
@@ -104,16 +120,18 @@ class LoadStoreQueue:
         return
 
     def __str__(self):
-        out_str= "===================Load/Store Queue===================\n")
-        out_str+="Op\t|\t|\tq_rs\t|\tq_rt|\tv_rs\t|\tv_rt\t|\tImmidiate\t|\tStr.Commit\t|\tCountdown")
-        out_str+="\n-------------------------------------------------------\n")
+        out_str= "============================== Load/Store Queue =============================\n"
+        out_str+="  Op |  q_rs  |  q_rt  |  v_rs  |  v_rt  | Str.Commit | Countdown | Immidiate\n"
+        out_str+="-----------------------------------------------------------------------------\n"
         for stat in self.queue_stations:
-            out_str += stat["op"] + "\t" + stat["qrs"] + "\t" + stat["qrt"] + \
-                       "\t" + str(stat["vrs"]) + "\t" + str(stat["vrt"]) + \
-                       str(stat["imm"]) + "\t" + str(stat["commit"]) + "\n"
-        out_str += "-------------------------------------------------------\n"
+            out_str += " " + stat["op"] + "  |  " + stat["qrs"] + "  |  " + \
+                       stat["qrt"] + "  |  " + str(stat["vrs"]) + "  |  " +  \
+                       str(stat["vrt"]) + "  |   " + str(stat["commit"]) +  \
+                       "    |     " + str(stat["countdown"]) + "     |   " + \
+                       str(stat["imm"]) + "\n"
+        out_str += "---------------------------------------------------------------------------\n"
         out_str += "Results Buffer: {}".format(self.result_buffer)
-
+        return out_str
 
 
 # stand alone rule about LSQ entry readiness
@@ -134,14 +152,15 @@ def lsq_entry_ready(entry):
 
 # Memory management class
 class Memory:
-    def __init__(self, size_bytes, word_len=4, mem_config=None, verbose=False):
+    def __init__(self, size_bytes, word_len=4, mem_config=None, verbose=True):
         self.mem_sz = size_bytes
         self.word_len = word_len
         self.memory = []
+        self.verbose = verbose
 
         if size_bytes % word_len != 0:
-            raise SegFaultException("On Initialization: Byte size of mem: " + \
-                      str(self.mem_sz) + "is not a mulitple of the word length: " +\
+            raise SegFaultException(" On Initialization: Byte size of mem: " + \
+                      str(self.mem_sz) + " is not a mulitple of the word length: " +\
                       str(word_len))
 
         self.init_mem(mem_config)
@@ -150,14 +169,15 @@ class Memory:
     # force initialize values into the memory block
     def init_mem(self, mem_arr):
         if mem_arr is None:
-            self.memory = [0x0] * (self.byte_addr % self.word_len)
-            if verbose:
+            self.memory = [0x0] * int(self.mem_sz / self.word_len)
+            if self.verbose:
                 print("[MEMRY]: Init'd clean memory. # Words: " + str(len(self.memory)))
 
         else:
+            print("with config")
             if len(mem_arr) != (self.mem_sz * self.word_len):
                 self.mem_sz = len(mem_sz) * self.word_len
-                if verbose:
+                if self.verbose:
                     print("[MEMRY]: Inputted Mem config did not expected size. " +\
                           "Size Param changed.")
             self.memory = mem_arr
@@ -168,23 +188,23 @@ class Memory:
     def access(self, io, byte_addr, value):
         # VALIDITY CHECKS
         if(byte_addr % self.word_len) != 0: # word alignment check
-            raise SegFaultException("Misaligned target addr [0x" + hex(byte_addr) + \
+            raise SegFaultException(" Misaligned target addr [" + hex(byte_addr) + \
                                     "] fails mem. word size: " + str(self.word_len))
 
         if byte_addr > (self.mem_sz - 1) or byte_addr < 0:  # Bounds check
-            raise SegFaultException("Out-of-Bounds Address: "+str(byte_addr)+ \
+            raise SegFaultException(" Out-of-Bounds Address: "+str(byte_addr)+ \
                                     "  is not [0: " + str(self.mem_sz-1) + "]")
 
         # ARRAY ACCESSES
         if io == "ld":
-            if verbose:
+            if self.verbose:
                 print("[MEMRY]: Accessed Load at EFF ADDR" + hex(byte_addr))
-            return self.memory[byte_addr % self.word_len]
+            return self.memory[int(byte_addr / self.word_len)]
 
         elif io == "Sd":
-            if verbose:
+            if self.verbose:
                 print("[MEMRY]: Storing value {} at {}", str(value), hex(byte_addr))
-            self.memory[byte_addr % self.word_len] = value
+            self.memory[int(byte_addr / self.word_len)] = value
 
         return None
 
@@ -195,39 +215,103 @@ class Memory:
 
 
     # prints current contents of memory
-    def print_mem(self, width=4):
+    def __str__(self, width=4):
+        item_sz = int(self.mem_sz / self.word_len)
+        mod_blck = item_sz - (item_sz % width)
+        remainder = item_sz % width
 
-        mod_blck = self.mem_sz - (self.mem_sz % width)
-        remainder = self.mem_sz % width
-
-        print("===Current Memory Configuration===")
+        output = "===Current Memory Configuration==="
         title_line = "\nOffset>"
         for i in range(width):
-            title_line += "\t0x" + hex(i*self.word_len)
-        print(title_line)
+            title_line += "\t" + hex(i*self.word_len)
+        output += title_line + "\n"
 
         for i in range(0, mod_blck, width):
-            mem_line = "0x" + hex(i*self.word_len)
+            mem_line = hex(i*self.word_len)
             for j in range(width):
                 mem_line += "\t" + str(self.memory[i+j])
-            print(mem_line)
+            output += mem_line + "\n"
 
-        rem_mem_line = "0x" + hex(mod_blck)
+        rem_mem_line = hex(mod_blck*self.word_len)
         for i in range(remainder):
             rem_mem_line += "\t" + str(self.memory[mod_blck+i])
-        print(rem_mem_line)
+        if remainder != 0:
+            output += rem_mem_line + "\n"
+        return output
 
 
 
 class SegFaultException(Exception):
     """ Raised when bad memory ops occur"""
     def __init__(self, message):
-        super().__init__("Seg Fault, core dumped:" + message)
+        super().__init__("Seg fault, core dumped:" + message)
 
+# =============================================================================
+#  Dummy units to test functionalities
+class test_rob:
+    def __init__(self, mem_ref):
+        self.cycle = -1
+        self.lsq = mem_ref
+
+
+    def request(self, register):
+        rob_reg = {"ROB1": None, "ROB2":4, "ROB3":None, "ROB4":None}
+        return rob_reg[register]
+
+    def tick(self):
+        self.cycle += 1
+        if self.cycle % 4 == 0:
+            self.lsq.mem_commit("ROB1")
+        else:
+            self.lsq.mem_commit(None)
+
+
+class test_cdb:
+    def __init__(self, sub):
+        self.sub = sub
+        self.cycles = 0
+
+    def tick(self):
+        self.cycles += 1
+        cdb_data = [None, {"dest":"ROB1", "value":13}, {"dest":"ROB3", "value":59}]
+        self.sub.read_cdb(cdb_data[self.cycles % 3])
 
 
 if __name__ == "__main__":
     print("Testing memory instructions")
-    instr_q = [Instruction("Sd", "ROB1", "ROB2", "0"),
-               Instruction("Ld", "ROB3", "ROB2", "0"),
-               Instruction("Sd", "ROB4", "ROB2", "3")]
+
+    inst = [{"op":"Sd", "rs":"ROB1", "rt":"ROB2", "imm":"0"},
+    {"op":"Ld", "rs":"ROB3", "rt":"ROB2", "imm":"0"},
+    {"op":"Sd", "rs":"ROB4", "rt":"ROB2", "imm":"0"}]
+
+    ld_str_q = LoadStoreQueue(26, 3, 4, None, wl=2) #bytes, queue, cyles
+    cdb = test_cdb(ld_str_q)
+    rob = test_rob(ld_str_q)
+    ld_str_q.reorder_buffer = rob
+    #print(ld_str_q)
+    #print(ld_str_q.mem_unit)
+
+    print("************************************")
+
+    ld_str_q.issue(inst[0])
+    ld_str_q.issue(inst[1])
+    ld_str_q.issue(inst[2])
+    print(ld_str_q)
+
+
+    ld_str_q.tick()
+    cdb.tick()
+    rob.tick()
+
+    ld_str_q.tick()
+    ld_str_q.tick()
+    ld_str_q.tick()
+    print(ld_str_q)
+
+    cdb.tick()
+    cdb.tick()
+    print(ld_str_q)
+
+    ld_str_q.tick()
+    print(ld_str_q)
+    print(ld_str_q.mem_unit)
